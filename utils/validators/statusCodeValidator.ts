64d775978137c7873c54e789cd5ec8be9f4ca0db
Ayush Chand <aayushchand900@gmail.com>
2025-12-31 02:08:54 +0530

    create repo via API

--- utils/validators/statusCodeValidator.ts
+++ utils/validators/statusCodeValidator.ts
@@ -1,7 +1,8 @@
 import {expect} from 'playwright/test'
 
 export class statusCodeValidator{
-    static async validateStatusCode(response, expectedStatusCode){
+
+    static async validateStatusCode(response: any, expectedStatusCode: number){
     expect(response.status()).toBe(expectedStatusCode);
   }
 }
